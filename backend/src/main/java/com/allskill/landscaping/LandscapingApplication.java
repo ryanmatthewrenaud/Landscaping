9eec3ae00831481c4fb049adb4d1666c1a452ecf
ryanmatthewrenaud <ryanmatthewrenaud@gmail.com>
2024-01-04 15:10:22 -0500

    Created basic project structure of landscaping, uses React and Spring.

---
+++ backend/src/main/java/com/allskill/landscaping/LandscapingApplication.java
@@ -0,0 +1,13 @@
+package com.allskill.landscaping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandscapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandscapingApplication.class, args);
+	}
+
+}
