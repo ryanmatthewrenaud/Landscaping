7ef85830f0e3ae27273e340d54ea760ce3919f6d
ryanmatthewrenaud <ryanmatthewrenaud@gmail.com>
2024-01-05 11:38:25 -0500

    Began creating classes for the project, that will create the backend.

---
+++ backend/src/main/java/com/allskill/contact/Contact.java
@@ -0,0 +1,12 @@
+package com.allskill.contact;
+
+public class Contact {
+
+    // Instance variables for contact [String:firstName, String:lastName,
+    // String:phoneNumber, Address:address, boolean:visited, boolean:contacted]
+
+    public Contact() {
+
+    }
+
+}
---
+++ backend/src/main/java/com/allskill/contact/location/Address.java
@@ -0,0 +1,20 @@
+package com.allskill.contact.location;
+
+public class Address {
+    // Instance variables for address [int:houseNumber, String:appartment
+    // String:streetName,
+    // String:city, String:state, int:zipCode]
+
+    private int houseNumber;
+    private String apartment;
+    private String streetName;
+    private String city;
+    private String state;
+    private int zipCode;
+
+    public Address(int houseNumber, String apartment, String streetName, String city, String state, int zipCode) {
+        super();
+
+    }
+
+}
